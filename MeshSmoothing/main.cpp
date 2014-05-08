cd88f83281da7654f129a6846d5156e348a2b6e5
李翔 <lixiangflyin@163.com>
2014-05-08 22:29:57 +0800

    Initial Commit

---
+++ MeshSmoothing/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MeshSmoothing
+//
+//  Created by apple on 14-5-8.
+//  Copyright (c) 2014年 apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
